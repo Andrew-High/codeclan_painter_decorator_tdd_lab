d93e541e03e621768cc77fb83c4f763f46d5fa7b
Andrew High <andrewhigh4@hotmail.com>
2021-05-04 15:59:06 +0100

    two tests passing

---
+++ models/room.js
@@ -0,0 +1,10 @@
+const Room = function(area, painted){
+    this.area = area;
+    this.painted = painted;
+};
+
+
+
+
+module.exports = Room;
+
---
+++ models/specs/room_specs.js
@@ -0,0 +1,25 @@
+const assert = require('assert');
+const Room = require('../room.js');
+
+
+
+describe('Room', function(){
+
+    let room;
+
+    beforeEach(function(){
+        room = new Room(10, false);
+    });
+
+    it('should have an area', function(){
+        const actual = room.area;
+        assert.strictEqual(actual, 10);
+    });
+
+    it("should start not painted", function(){
+        const actual = room.painted;
+        assert.strictEqual(actual, false);
+    });
+
+
+});
